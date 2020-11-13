77d807d7efcbb9c9cbd27bc2aed01d9f55794e4f
lzy3me <fluke.workontime@gmail.com>
2020-11-14 02:25:55 +0700

    adding github action. change in vue config file.

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,8 @@
-const { config } = require("vue/types/umd");
-
 module.exports = {
-    chainWebpack: config => {
-        config.plugin('html').tap(args => {
-            args[0].title = "LzyDev @lzy3me"
-            return args
-        })
-    }
-}
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].title = "LzyDev @lzy3me";
+      return args;
+    });
+  }
+};
