00aa106ef204541f13ff44eb5c482c28f56efd43
lzy3me <fluke.workontime@gmail.com>
2020-11-14 01:40:27 +0700

    Initialize new branch for new web framework.

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const { config } = require("vue/types/umd");
+
+module.exports = {
+    chainWebpack: config => {
+        config.plugin('html').tap(args => {
+            args[0].title = "LzyDev @lzy3me"
+            return args
+        })
+    }
+}
